f618cd398585e8a0176398e2c3f3755a0f7279b6
MEMO <152151374+Memough@users.noreply.github.com>
2025-12-07 16:50:42 -0600

    Main modificado

--- main.cpp
+++ main.cpp
@@ -9,16 +9,38 @@ int main() {
 
     while (true) {
         cout << "\n========== MENU PRINCIPAL ==========\n";
-        cout << "1) Problema de Distribucion de Recurso\n";
-        cout << "2) Problema del Vendedor (TSP)\n";
+        cout << "1) Problema de Asignacion 1 a 1\n";
+        cout << "2) Problema de Distribucion de Recurso\n";
+        cout << "3) Problema de la Mochila\n";
+        cout << "4) Problema del Vendedor\n";
         cout << "0) Salir\n";
         cout << "Seleccione opcion: ";
         cin >> op;
 
-        if (op == 1) menuRecurso();
-        else if (op == 2) menuTSP();
-        else if (op == 0) break;
-        else cout << "Opcion invalida.\n";
+        switch (op) {
+            case 1:
+                cout << "\n No implementado.\n";
+                break;
+
+            case 2:
+                menuRecurso();   
+                break;
+
+            case 3:
+                cout << "\n No implementado.\n";
+                break;
+
+            case 4:
+                menuTSP();   
+                break;
+
+            case 0:
+                return 0;
+
+            default:
+                cout << "Opcion invalida.\n";
+                break;
+        }
     }
 
     return 0;
